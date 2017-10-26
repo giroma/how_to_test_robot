a98ca76839de0ec034fce6c1410ce85e134c347d
Argir Argirov <dj.giro@gmail.com>
2017-10-26 14:49:21 -0400

    done

--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,102 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
+
     # arrange
-
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = true
     # act
-
+    actual = robot.station
     # assert
+    expected = 1
+    assert_equal(actual, expected)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.vintage_model = true
+    robot.needs_repairs = true
     # act
-
+    actual = robot.station
     # assert
+    expected = 2
+    assert_equal(actual, expected)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
+    robot = Robot.new
 
+    robot.needs_repairs = true
     # act
-
+    actual = robot.station
     # assert
+    expected = 3
+    assert_equal(actual, expected)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = false
     # act
-
+    actual = robot.station
     # assert
+    expected = 4
+    assert_equal(actual, expected)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = true
     # act
-
+    actual = robot.prioritize_tasks
     # assert
+    eexpected = -1
+    assert_equal(eexpected, actual )
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = true
 
+    robot.todos = [12]
     # act
-
+    actual = robot.prioritize_tasks
     # assert
+    eexpected = robot.todos.max
+    assert_equal(eexpected, actual )
   end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
+    robot = Robot.new
+    robot.day_off = "Sunday"
 
     # act
-
+    actual = robot.workday?("Sunday")
     # assert
+    eexpected = false
+    assert_equal(eexpected, actual )
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
+    robot = Robot.new
+    robot.day_off = "Sunday"
 
     # act
-
+    actual = robot.workday?("Friday")
     # assert
+    eexpected = true
+    assert_equal(eexpected, actual )
   end
 
 end
